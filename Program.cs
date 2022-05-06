6be5ad01674fd1feaf076e928b1faddccb734051
Yusuf S. Kaygusuz <86704802+YusufsKaygusuz@users.noreply.github.com>
2022-05-06 22:58:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bubble_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Numbers = new int[] {12,8,27,2,6,19,10 };
+
+            for(int i =0; i<Numbers.Length - 1; i++)
+            {
+                for (int j = 1; j < Numbers.Length - i; j++)
+                {
+                    if(Numbers[j]< Numbers[j-1])
+                    {
+                        int temp = Numbers[j - 1];
+                        Numbers[j - 1] = Numbers[j];
+                        Numbers[j] = temp;
+                    }
+                }
+            }
+
+            for (int i = 0; i < Numbers.Length; i++)
+            {
+                Console.WriteLine(Numbers[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
